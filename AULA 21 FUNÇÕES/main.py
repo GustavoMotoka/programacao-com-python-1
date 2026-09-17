68df09646f4397f0550f998296cccfa63abac51f
GustavoMotoka <gustavo.mota.filho@escola.pr.gov.br>
2026-09-17 10:45:01 -0300

    Add files via upload

---
+++ AULA 21 FUNÇÕES/main.py
@@ -0,0 +1,13 @@
+print()
+int()
+
+def calcular_media():
+    print("Calculando A média...")
+
+def saudacao(nome):
+    print(f"bem vindo! {nome}")
+    
+calcular_media()
+saudacao('Cristian')
+saudacao('Jonatan')
+saudacao('fefe')
