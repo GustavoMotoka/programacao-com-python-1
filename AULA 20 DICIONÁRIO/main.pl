33d7f5429c8253afb326c83a0f81058822196905
GustavoMotoka <gustavo.mota.filho@escola.pr.gov.br>
2026-09-17 10:44:41 -0300

    Add files via upload

---
+++ AULA 20 DICIONÁRIO/main.pl
@@ -0,0 +1,9 @@
+personagem = [
+    {'nome': 'Bob Esponja', 'idade': 40},
+    {"nome": 'Thanos', 'idade': 1000}
+]
+
+for personagem in personagem:
+    print("nome:", personagem['nome'])
+    print("idade:", personagem['idade'])
+    print("-------------------")
